3039949e207175a0d76fc38a847ad49bb16ead56
ruizii <santiago-rb1@outlook.es>
2024-05-23 18:43:23 -0500

    Added keymap and fixed dependency on toggleterm

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -6,8 +6,8 @@ vim.keymap.set("n", "gc", "<nop>")
 vim.keymap.set({ "i", "x" }, "<C-s>", "<ESC><cmd>silent! update | redraw<CR>", { noremap = true, silent = true })
 
 -- Buffer navigation
-vim.keymap.set("n", "<leader>h", "<cmd>bprev<CR>", { remap = true })
-vim.keymap.set("n", "<leader>l", "<cmd>bnext<CR>", { remap = true })
+vim.keymap.set("n", "H", "<cmd>bprev<CR>", { remap = true })
+vim.keymap.set("n", "L", "<cmd>bnext<CR>", { remap = true })
 vim.keymap.set("n", "<leader>c", "<cmd>bdelete<CR>", { noremap = true })
 
 -- Telescope
@@ -16,3 +16,7 @@ vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { remap = true
 vim.keymap.set("n", "<leader><leader>", "<cmd>Telescope buffers<CR>", { remap = true })
 vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { remap = true })
 vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { remap = true })
+vim.keymap.set("n", "<leader>fz", "<cmd>Telescope zoxide list<CR>", { remap = true })
+
+-- Code Runner
+vim.keymap.set("n", "<F5>", "<cmd>RunCode<CR>", { noremap = true, silent = false })
--- lua/plugins/code-runner.lua
+++ lua/plugins/code-runner.lua
@@ -1,21 +1,37 @@
 return {
 	"CRAG666/code_runner.nvim",
-	cmd = "RunCode",
-	opts = {
-		mode = "toggleterm",
-		startinsert = true,
-		term = {
-			size = 15,
-		},
-		filetype = {
-			python = "python -u",
-			javascript = "node",
-			cpp = "cd $dir && make && $dir/$fileNameWithoutExt",
-			c = "cd $dir && make && $dir/$fileNameWithoutExt",
-			go = "go run",
-			sh = "bash",
-			asm = "nasm -f elf64 $fileName && ld $fileNameWithoutExt.o -o $fileNameWithoutExt && ./$fileNameWithoutExt",
-			lua = "lua",
+	dependencies = {
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		cmd = { "RunCode", "RunFile", "RunProject" },
+		keys = { "<C-t>" },
+		opts = {
+			open_mapping = [[<C-t>]],
+			hide_numbers = true,
+			autochdir = true,
+			insert_mappings = true,
+			shade_terminals = false,
+			close_on_exit = false,
 		},
 	},
+	cmd = { "RunCode", "RunFile", "RunProject" },
+	config = function()
+		require("code_runner").setup({
+			mode = "toggleterm",
+			startinsert = true,
+			term = {
+				size = 15,
+			},
+			filetype = {
+				python = "python -u",
+				javascript = "node",
+				cpp = "cd $dir && make && $dir/$fileNameWithoutExt",
+				c = "cd $dir && make && $dir/$fileNameWithoutExt",
+				go = "go run",
+				sh = "bash",
+				asm = "nasm -f elf64 $fileName && ld $fileNameWithoutExt.o -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+				lua = "lua",
+			},
+		})
+	end,
 }
