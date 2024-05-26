8e0f17ddd0ebb6023511423ed9e246bee2d06105
ruizii <santiago-rb1@outlook.es>
2024-05-26 14:26:25 -0500

    Lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,5 +2,12 @@ return {
 	"nvim-lualine/lualine.nvim",
 	event = { "BufReadPost", "BufNewFile" },
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = true,
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "onedark",
+				-- ... your lualine config
+			},
+		})
+	end,
 }
