6f98970679b99c1a2f1bff0953f4ae545382f765
ruizii <santiago-rb1@outlook.es>
2024-05-26 21:47:40 -0500

    Added harpoonline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,19 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	event = { "BufReadPost", "BufNewFile" },
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "nvim-tree/nvim-web-devicons", "abeldekat/harpoonline" },
 	config = function()
+		local Harpoonline = require("harpoonline")
+		Harpoonline.setup({
+			on_update = function()
+				require("lualine").refresh()
+			end,
+		})
+		local lualine_c = { Harpoonline.format, "filename" }
 		require("lualine").setup({
+			sections = {
+				lualine_c = lualine_c,
+			},
 			options = {
 				theme = "onedark",
 			},
