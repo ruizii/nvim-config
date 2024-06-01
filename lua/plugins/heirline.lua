28e62c372115c46411850cd9412d7a7af3619fc6
ruizii <santiago-rb1@outlook.es>
2024-06-01 02:41:52 -0500

    Fix heirline FileSize formatting

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -188,15 +188,14 @@ return {
 
 		local FileSize = {
 			provider = function()
-				-- stackoverflow, compute human readable file size
 				local suffix = { "b", "k", "M", "G", "T", "P", "E" }
 				local fsize = vim.fn.getfsize(vim.api.nvim_buf_get_name(0))
 				fsize = (fsize < 0 and 0) or fsize
 				if fsize < 1024 then
-					return fsize .. suffix[1]
+					return string.format(" [%.2d%s]", fsize, suffix[1])
 				end
 				local i = math.floor((math.log(fsize) / math.log(1024)))
-				return " [" .. string.format("%.2g%s", fsize / math.pow(1024, i), suffix[i + 1]) .. "]"
+				return string.format(" [%.2g%s]", fsize / math.pow(1024, i), suffix[i + 1])
 			end,
 			hl = { fg = "blue" },
 		}
@@ -206,7 +205,6 @@ return {
 			FileIcon,
 			utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
 			FileFlags,
-			FileSize,
 			{ provider = "%<" } -- this means that the statusline is cut here when there's not enough space
 		)
 
@@ -396,6 +394,7 @@ return {
 			Space,
 			Space,
 			FileNameBlock,
+			FileSize,
 			Space,
 			Space,
 			Git,
