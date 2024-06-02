a0c83fe56054c13fad29b9ae61f97bf48a191ffd
ruizii <santiago-rb1@outlook.es>
2024-06-02 18:03:39 -0500

    Change illuminate event

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,7 +1,6 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
 	{
 		"lewis6991/gitsigns.nvim",
-		lazy = true,
 		event = { "BufReadPost", "BufNewFile" },
 		opts = {
 			signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
@@ -15,23 +14,12 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
 	},
 	{
 		"RRethy/vim-illuminate",
-		event = { "InsertEnter" },
-
+		event = { "BufReadPost", "BufNewFile" },
 		config = function()
 			-- change the highlight style
 			vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
 			vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
 			vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
-
-			--- auto update the highlight style on colorscheme change
-			vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-				pattern = { "*" },
-				callback = function()
-					vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-					vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
-					vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
-				end,
-			})
 
 			require("illuminate").configure({
 				delay = 400,
@@ -41,7 +29,6 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
 				},
 				filetypes_denylist = {
 					"TelescopePrompt",
-					"neo-tree",
 				},
 				min_count_to_highlight = 2,
 			})
