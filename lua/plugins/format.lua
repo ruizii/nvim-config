d57a2d1397c14e2de52082de213a6d29628bd7ef
ruizii <santiago-rb1@outlook.es>
2024-07-08 13:01:44 -0500

    Replace ruff with pyright

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -10,7 +10,7 @@ return {
 		},
 		formatters_by_ft = {
 			lua = { "stylua" },
-			python = { "ruff_format" },
+			python = { "isort", "black" },
 			c = { "clang-format" },
 			cpp = { "clang-format" },
 			go = { "goimports", "gofmt" },
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -103,13 +103,6 @@ return {
 			sign({ name = "DiagnosticSignHint", text = "󰠠" })
 			sign({ name = "DiagnosticSignInfo", text = "" })
 
-			local on_attach = function(client, bufnr)
-				if client.name == "ruff_lsp" then
-					-- Disable hover in favor of Pyright
-					client.server_capabilities.hoverProvider = false
-				end
-			end
-
 			lspconfig.lua_ls.setup({
 				lspconfig.lua_ls.setup({
 					capabilities = capabilities,
@@ -185,19 +178,14 @@ return {
 				},
 			})
 
-			lspconfig.ruff.setup({
-				on_attach = on_attach,
-			})
-
 			lspconfig.pyright.setup({
 				settings = {
-					pyright = {
-						disableOrganizeImports = true,
-					},
-				},
-				python = {
-					analysis = {
-						ignore = { "*" },
+					python = {
+						analysis = {
+							diagnosticSeverityOverrides = {
+								reportWildcardImportFromLibrary = "none",
+							},
+						},
 					},
 				},
 			})
