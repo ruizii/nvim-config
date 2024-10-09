00dee70c9c25ec98e460758827db004e08e1e1ea
ruizii <santiago-rb1@outlook.es>
2024-10-08 23:24:41 -0500

    Remove mason ensure_installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,18 +7,6 @@ return {
 			ui = {
 				border = "single",
 			},
-			ensure_installed = {
-				"bashls",
-				"clangd",
-				"cssls",
-				"emmet_ls",
-				"html",
-				"jsonls",
-				"lua_ls",
-				"pyright",
-				"tsserver",
-				"eslint",
-			},
 		},
 	},
 }
