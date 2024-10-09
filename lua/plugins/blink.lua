6d57053b140ab2a513731e1b7760c4e9076c81ed
ruizii <santiago-rb1@outlook.es>
2024-10-09 15:14:21 -0500

    Formatting

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,12 +1,8 @@
 return {
 	"saghen/blink.cmp",
 	lazy = false,
-	-- optional: provides snippets for the snippet source
 	dependencies = "rafamadriz/friendly-snippets",
-
-	-- use a release tag to download pre-built binaries
 	version = "v0.*",
-
 	opts = {
 		keymap = {
 			show = {},
