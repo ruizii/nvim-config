aff8175739c2830abcee5874266909acc461b243
ruizii <santiago-rb1@outlook.es>
2024-10-08 23:48:19 -0500

    Remove cmp & simplify autopairs config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,7 @@
 return {
 	"saghen/blink.cmp",
-	lazy = false,
+	event = "User FilePost",
+	-- lazy = false,
 	-- optional: provides snippets for the snippet source
 	dependencies = "rafamadriz/friendly-snippets",
 
