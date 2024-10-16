32dbd422e88cd268d114f1a67238f11777717ebc
ruizii <santiago-rb1@outlook.es>
2024-10-16 15:15:45 -0500

    Improve treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,24 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	version = false,
+	build = ":TSUpdate",
 	event = { "BufReadPost", "BufNewFile" },
+	lazy = vim.fn.argc(-1) == 0,
+	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+
 	dependencies = {
 		"HiPhish/rainbow-delimiters.nvim",
 		main = "rainbow-delimiters.setup",
 	},
-	version = false,
-	build = ":TSUpdate",
-	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-	opts = function()
-		local options = {
+
+	init = function(plugin)
+		require("lazy.core.loader").add_to_rtp(plugin)
+		require("nvim-treesitter.query_predicates")
+	end,
+
+	config = function()
+		---@diagnostic disable-next-line
+		require("nvim-treesitter.configs").setup({
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = false,
@@ -17,10 +26,6 @@ return {
 			indent = {
 				enable = true,
 			},
-		}
-		return options
-	end,
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
+		})
 	end,
 }
