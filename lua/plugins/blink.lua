7feea0af5aa54df3226c2ebc47816ce6afa2cf70
ruizii <santiago-rb1@outlook.es>
2024-12-02 22:21:32 -0500

    Fix blink config changes

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,70 +4,72 @@ return {
 	dependencies = "rafamadriz/friendly-snippets",
 	version = "v0.*",
 	opts = {
-		keymap = "default",
-		-- keymap = {
-		-- 	preset = "default",
-		-- 	["<C-u>"] = { "scroll_documentation_up", "fallback" },
-		-- 	["<C-d>"] = { "scroll_documentation_down", "fallback" },
-		-- },
-		highlight = {
-			use_nvim_cmp_as_default = false,
+		keymap = {
+			preset = "default",
+			["<C-u>"] = { "scroll_documentation_up", "fallback" },
+			["<C-d>"] = { "scroll_documentation_down", "fallback" },
 		},
-		windows = {
-			autocomplete = {
+
+		blocked_filetypes = { "text" },
+
+		completion = {
+			trigger = {
+				show_in_snippet = false,
+			},
+			menu = {
 				min_width = 10,
+				max_height = 10,
 				border = "single",
 				draw = {
-					columns = { { "label", "label_description" }, { "kind_icon" } },
+					columns = { { "label", "label_description", gap = 1 }, { "kind_icon" } },
 				},
 				scrolloff = 0,
 			},
 			documentation = {
-				border = "single",
-				auto_show = true,
-				auto_show_delay_ms = 50,
-				max_width = 80,
+				window = {
+					max_width = 80,
+					border = "single",
+				},
+			},
+			accept = {
+				auto_brackets = {
+					enabled = true,
+				},
 			},
 		},
-
 		-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-		nerd_font_variant = "mono",
-		kind_icons = {
-			Function = "λ",
-			Snippet = "",
-			Text = "󰉿",
-			Method = "󰆧",
-			Constructor = "",
-			Field = "󰜢",
-			Variable = "󰀫",
-			Class = "󰠱",
-			Interface = "",
-			Module = "",
-			Property = "󰜢",
-			Unit = "󰑭",
-			Value = "󰎠",
-			Enum = "",
-			Keyword = "󰌋",
-			Color = "󰏘",
-			File = "󰈙",
-			Reference = "󰈇",
-			Folder = "󰉋",
-			EnumMember = "",
-			Constant = "󰏿",
-			Struct = "󰙅",
-			Event = "",
-			Operator = "󰆕",
-			TypeParameter = "",
-		},
 
-		-- experimental auto-brackets support
-		accept = {
-			auto_brackets = {
-				enabled = true,
+		nerd_font_variant = "mono",
+		appearance = {
+			use_nvim_cmp_as_default = false,
+
+			kind_icons = {
+				Function = "λ",
+				Snippet = "",
+				Text = "󰉿",
+				Method = "󰆧",
+				Constructor = "",
+				Field = "󰜢",
+				Variable = "󰀫",
+				Class = "󰠱",
+				Interface = "",
+				Module = "",
+				Property = "󰜢",
+				Unit = "󰑭",
+				Value = "󰎠",
+				Enum = "",
+				Keyword = "󰌋",
+				Color = "󰏘",
+				File = "󰈙",
+				Reference = "󰈇",
+				Folder = "󰉋",
+				EnumMember = "",
+				Constant = "󰏿",
+				Struct = "󰙅",
+				Event = "",
+				Operator = "󰆕",
+				TypeParameter = "",
 			},
 		},
-
-		-- experimental signature help support
-		-- trigger = { signature_help = { enabled = true } },
 	},
 }
