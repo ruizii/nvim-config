1a9839d24667e58638da33b4abc63497131d8926
ruizii <santiago-rb1@outlook.es>
2025-02-11 22:57:45 -0500

    Disable unused themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -3,7 +3,6 @@ return {
 
 		"navarasu/onedark.nvim",
 		priority = 1000,
-		enabled = true,
 		opts = {
 			style = "dark",
 			highlights = {
@@ -25,7 +24,6 @@ return {
 	},
 	{
 		"wnkz/monoglow.nvim",
-		lazy = false,
 		priority = 1000,
 		opts = {},
 	},
