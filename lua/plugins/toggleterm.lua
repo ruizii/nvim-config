3146e38cb90ccb34fdd1ff445be92f7ad481d87d
ruizii <santiago-rb1@outlook.es>
2025-02-23 15:30:53 -0500

    Redundant table

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,19 +1,17 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		keys = {
-			{
-				"<C-t>",
-				"<cmd>ToggleTerm<cr>",
-				desc = "Toggle terminal",
-				mode = { "n", "t" },
-			},
+	"akinsho/toggleterm.nvim",
+	keys = {
+		{
+			"<C-t>",
+			"<cmd>ToggleTerm<cr>",
+			desc = "Toggle terminal",
+			mode = { "n", "t" },
 		},
-		opts = {
-			direction = "float",
-			size = 18,
-			hide_numbers = true,
-			start_in_insert = true,
-		},
+	},
+	opts = {
+		direction = "float",
+		size = 18,
+		hide_numbers = true,
+		start_in_insert = true,
 	},
 }
